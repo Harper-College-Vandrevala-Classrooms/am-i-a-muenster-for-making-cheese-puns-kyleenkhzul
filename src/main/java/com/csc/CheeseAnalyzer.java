eff7eb79afd9c176843e60b33f368f454c506039
kyleenkhzul <90794184+kyleenkhzul@users.noreply.github.com>
2024-10-05 21:28:16 -0500

    a

--- src/main/java/com/csc/CheeseAnalyzer.java
+++ src/main/java/com/csc/CheeseAnalyzer.java
@@ -8,101 +8,99 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 
 public class CheeseAnalyzer {
-  String fileName;
-  int number_of_pastuerized_milk = 0,
-      number_of_raw_milk = 0,
-      number_of_organic_cheese_moisture_greater_than_forty_percent = 0,
-      number_of_cheese_from_cow = 0,
-      number_of_cheese_from_goat = 0,
-      number_of_cheese_from_ewe = 0,
-      number_of_cheese_from_buffalo = 0;
-  
-  public void analyzeCheeseData(String fileName) {
-    try {
-      BufferedReader reader = new BufferedReader(new FileReader(fileName));
-      String line = reader.readLine(); // skips header line
+    String fileName;
+    int number_of_pasteurized_milk = 0,
+        number_of_raw_milk = 0,
+        number_of_organic_cheese_moisture_greater_than_forty_percent = 0,
+        number_of_cheese_from_cow = 0,
+        number_of_cheese_from_goat = 0,
+        number_of_cheese_from_ewe = 0,
+        number_of_cheese_from_buffalo = 0;
 
-      while((line = reader.readLine()) != null) {
-        String[] data = line.split(",");
-        
-        String milkTreatmentType = data[9];
-        double moisturePercent = Double.parseDouble(data[3]);
-        int organic = Integer.parseInt(data[6]);
-        String animal = data[8].toLowerCase();
+    public void analyzeCheeseData(String fileName) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            String line = reader.readLine();
 
-        if(milkTreatmentType.equals("Raw Milk")) {
-          number_of_raw_milk++;
+            while ((line = reader.readLine()) != null) {
+
+                String[] data = line.split(",");
+                String milkTreatmentType = data[9];
+                double moisturePercent = Double.parseDouble(data[3]);
+                int organic = Integer.parseInt(data[6]);
+                String animal = data[8].toLowerCase();
+
+                if (milkTreatmentType.equals("Raw Milk")) {
+                    number_of_raw_milk++;
+                } else {
+                    number_of_pasteurized_milk++;
+                }
+
+                if (moisturePercent > 41.0 && organic == 1) {
+                    number_of_organic_cheese_moisture_greater_than_forty_percent++;
+                }
+
+                switch (animal) {
+                    case "cow":
+                        number_of_cheese_from_cow++;
+                        break;
+                    case "goat":
+                        number_of_cheese_from_goat++;
+                        break;
+                    case "ewe":
+                        number_of_cheese_from_ewe++;
+                        break;
+                    case "buffalo":
+                        number_of_cheese_from_buffalo++;
+                        break;
+                    default:
+                        System.err.println("Unknown animal type: " + animal);
+                        break;
+                }
+            }
+            reader.close();
+        } catch (FileNotFoundException err) {
+            err.printStackTrace();
+        } catch (IOException | NumberFormatException d) {
+            d.printStackTrace();
+        }
+    }
+
+    public void writeAnalysisResult() {
+        try {
+            String mostAnimal = compareMostCheesesFromAnAnimal(
+                number_of_cheese_from_cow,
+                number_of_cheese_from_goat,
+                number_of_cheese_from_ewe,
+                number_of_cheese_from_buffalo
+            );
+
+            BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
+            writer.write("The number of cheeses that use pasteurized milk is: " + number_of_pasteurized_milk);
+            writer.write("\nThe number of cheeses that use raw milk is: " + number_of_raw_milk);
+            writer.write("\nThe number of organic cheeses that have a moisture percentage greater than 41.0% is: " + number_of_organic_cheese_moisture_greater_than_forty_percent);
+            writer.write("\nThe animal that most cheeses in Canada comes from is: " + mostAnimal);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String compareMostCheesesFromAnAnimal(int cow, int goat, int ewe, int buffalo) {
+        if (cow > goat && cow > ewe && cow > buffalo) {
+            return "Cows";
+        } else if (goat > cow && goat > ewe && goat > buffalo) {
+            return "Goats";
+        } else if (ewe > cow && ewe > goat && ewe > buffalo) {
+            return "Ewes";
         } else {
-          number_of_pastuerized_milk++;
+            return "Buffalos";
         }
+    }
 
-        if(moisturePercent > 41.0 && organic == 1) {
-          number_of_organic_cheese_moisture_greater_than_forty_percent++;
-        }
-
-        switch (animal) {
-          case "cow":
-              number_of_cheese_from_cow++;
-              break;
-          case "goat":
-              number_of_cheese_from_goat++;
-              break;
-          case "ewe": 
-              number_of_cheese_from_ewe++;
-              break;
-          case "buffalo": 
-              number_of_cheese_from_buffalo++;
-              break;
-        }
-      }
-      reader.close();
-    } 
-  catch(FileNotFoundException err) { 
-      err.printStackTrace();
-  } 
-  catch(IOException d) {
-      d.printStackTrace();
-  }
-}
-
-  public void writeAnalysisResult() {
-  try {
-    String mostAnimal = compareMostCheesesFromAnAnimal(
-      number_of_cheese_from_cow, 
-      number_of_cheese_from_goat, 
-      number_of_cheese_from_ewe, 
-      number_of_cheese_from_buffalo
-    );
-
-    BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
-    writer.write("The number of cheeses that use pasteurized milk is: " + number_of_pastuerized_milk);
-    writer.write("The number of cheese that use raw milk is: " + number_of_raw_milk);
-    writer.write("The number of organic cheeses that have a moisture percentage greather than 41.0% is: " + number_of_organic_cheese_moisture_greater_than_forty_percent);
-    writer.write("The animal that most cheeses in Canada comes from are: " + mostAnimal);
-    writer.close();
-  } catch (IOException e) {
-    e.printStackTrace();
-  }
-}
-
-public String compareMostCheesesFromAnAnimal(int cow, int goat, int ewe, int buffalo) {
-  if(cow > goat && cow > ewe && cow > buffalo) {
-    return "Cows";
-  }
-  else if(goat > cow && goat > ewe && goat > buffalo) {
-    return "Goats";
-  }
-  else if(ewe > cow && ewe > goat && ewe > buffalo) {
-    return "Ewes";
-  }
-  else {
-    return "Buffalos";
-  }
-}
-
-public static void main(String[] args) {
-  CheeseAnalyzer analyzer = new CheeseAnalyzer();
-  analyzer.analyzeCheeseData("cheese_data.csv");
-  analyzer.writeAnalysisResult();
-}
+    public static void main(String[] args) {
+        CheeseAnalyzer analyzer = new CheeseAnalyzer();
+        analyzer.analyzeCheeseData("cheese_data.csv");
+        analyzer.writeAnalysisResult();
+    }
 }
